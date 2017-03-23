9a60e52f088e1354cbb5b15c5c0821acf1dbadce
Ian Beamish <ianbeami@gmail.com>
2017-03-23 15:34:45 +0800

    messages, conversation MVC

--- AirAlien/app/controllers/conversations_controller.rb
+++ AirAlien/app/controllers/conversations_controller.rb
@@ -2,7 +2,7 @@ class ConversationsController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-		@users =User.all? 
+		@users = User.all
 		@conversations = Conversation.involving(current_user)
 	end
 
@@ -10,7 +10,7 @@ class ConversationsController < ApplicationController
 		if Conversation.between(params[:sender_id], params[:recipient_id]).present?
 			@conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
 		else
-			@conversation = Coversation.create(coversation_params)
+			@conversation = Conversation.create(conversation_params)
 		end
 
 		redirect_to conversation_messages_path(@conversation)
